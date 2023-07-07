9c6ae86cb21f42500abc962f2d3e3aa42295b21b
Sivakumar Balasubramanian <siva82kb@gmail.com>
2023-07-07 16:11:30 +0530

    Got the VM and AM code cleaned up a little. Implemented causal and non-causal versions wherever needed. Mismatch between the number of points for AM and VM needs to be addressed.

--- scripts/misc.py
+++ scripts/misc.py
@@ -11,26 +11,31 @@ from ahrs.filters import Madgwick
 from ahrs.common import orientation
 
 
-def computer_tilt_for_all_subjects(alldf: pd.DataFrame, accl_lbl: str, nwin: int) -> dict:
+def computer_tilt_for_all_subjects(alldf: pd.DataFrame, accl_lbl: str, nwin: int, causal: bool=True) -> dict:
     """
     Computes the tilt angle for all subjects in the given dataframe.
     """
     return {
         _subj: np.hstack([
             compute_tilt(alldf.loc[(alldf.loc[:, 'subject'] == _subj) &
-                                   (alldf.loc[:, 'segment'] == seg), accl_lbl], nwin)
+                                   (alldf.loc[:, 'segment'] == seg), accl_lbl],
+                         nwin, causal)
             for seg in alldf.loc[alldf.loc[:, 'subject'] == _subj, 'segment'].unique()])
         for _subj in np.unique(alldf.subject)
     }
 
 
-def compute_tilt(accl_farm: np.array, nwin: int) -> np.array:
+def compute_tilt(accl_farm: np.array, nwin: int, causal: bool=True) -> np.array:
     """
     Computes the tilt angle from the accelerometer data.
     """
-    # Moving averaging using the Savitzky-Golay filter
-    acclf = signal.savgol_filter(accl_farm, window_length=nwin, polyorder=0,
-                                 mode='constant')
+    if causal:
+        # Moving averaging using the causal filter
+        acclf = signal.lfilter(np.ones(nwin) / nwin, 1, accl_farm)
+    else:
+        # Moving averaging using the Savitzky-Golay filter
+        acclf = signal.savgol_filter(accl_farm, window_length=nwin, polyorder=0,
+                                    mode='constant')
     acclf[acclf < -1] = -1
     acclf[acclf > 1] = 1
     return -np.rad2deg(np.arccos(acclf)) + 90
@@ -82,34 +87,7 @@ def assign_segments(df: pd.DataFrame, dur_th:float = 1, dT: float = 0.02) -> pd.
     return pd.concat([df, _segdf], axis=1)
 
 
-def get_largest_continuous_segment_indices(data: pd.DataFrame, subject: int,
-                                           deltaT: np.timedelta64) -> tuple[int, int]:
-    """
-    Returns the indices of the longest continuous segment of data for the
-    given subject.
-    """
-    _dtimes = np.diff(data[data.subject == subject].index)
-    _jumpinx = np.hstack(([0], np.where(_dtimes > deltaT)[0]))
-    _inx1 = np.argmax(np.diff(_jumpinx))
-    return _jumpinx[_inx1], _jumpinx[_inx1 + 1] + 1
-
-
-def get_segmented_data(data: pd.DataFrame, deltaT: np.timedelta64) -> dict[int, pd.DataFrame]:
-    """
-    Returns the segmented data for each subject in the given data.
-    """
-    # Go through each subject and get the longest continuous segments of data
-    subjs = np.unique(data.subject)
-    # Segment left data
-    _subjdata = {}
-    for _subj in subjs:
-        # Get indices of the longest continuous segment of data
-        inx = get_largest_continuous_segment_indices(data, _subj, deltaT)
-        _subjdata[_subj] = data[data.subject == _subj].iloc[inx[0]:inx[1]]
-    return _subjdata
-
-
-def get_continuous_segments(df):
+def get_continuous_segments(df: pd.DataFrame) -> list[pd.DataFrame]:
     # returns a list of continuous sections (as dataframes) from the original dataframe
 
     time_diff = np.array([pd.Timedelta(diff).total_seconds()
@@ -127,7 +105,6 @@ def compute_vector_magnitude(imudf: pd.DataFrame) -> pd.DataFrame:
     """
     imudf = imudf.loc[:, ['ax', 'ay', 'az', 'gx', 'gy', 'gz']]
     imudf = resample(imudf, 30)
-    op_df = pd.DataFrame(index=imudf.index)
 
     gyr = np.array(imudf[['gx', 'gy', 'gz']])
     acc = np.array(imudf[['ax', 'ay', 'az']])
@@ -146,27 +123,32 @@ def compute_vector_magnitude(imudf: pd.DataFrame) -> pd.DataFrame:
         r = orientation.q2R(q[i])
         ae[i] = np.matmul(r, acc[i]) - g
 
-    op_df['ax'] = bandpass(np.nan_to_num(ae[:, 0]), fs=30)
-    op_df['ay'] = bandpass(np.nan_to_num(ae[:, 1]), fs=30)
-    op_df['az'] = bandpass(np.nan_to_num(ae[:, 2]), fs=30)
-    op_df = resample(op_df, 10)
+    out_df = pd.DataFrame(index=imudf.index)
+    out_df['ax'] = bandpass(np.nan_to_num(ae[:, 0]), fs=30)
+    out_df['ay'] = bandpass(np.nan_to_num(ae[:, 1]), fs=30)
+    out_df['az'] = bandpass(np.nan_to_num(ae[:, 2]), fs=30)
+    out_df = resample(out_df, 10)
 
-    op_df['ax'] = np.where(np.absolute(op_df['ax'].values) < 0.068, 0, op_df['ax'].values) / 0.01664
-    op_df['ay'] = np.where(np.absolute(op_df['ay'].values) < 0.068, 0, op_df['ay'].values) / 0.01664
-    op_df['az'] = np.where(np.absolute(op_df['az'].values) < 0.068, 0, op_df['az'].values) / 0.01664
+    out_df['ax'] = np.where(np.absolute(out_df['ax'].values) < 0.068, 0, out_df['ax'].values) / 0.01664
+    out_df['ay'] = np.where(np.absolute(out_df['ay'].values) < 0.068, 0, out_df['ay'].values) / 0.01664
+    out_df['az'] = np.where(np.absolute(out_df['az'].values) < 0.068, 0, out_df['az'].values) / 0.01664
 
-    dfs = get_continuous_segments(op_df)
+    dfs = get_continuous_segments(out_df)
     dfs = [df.resample(str(1) + 'S').sum() for df in dfs]
-    op_df = pd.concat(dfs)
-    op_df.index.name = 'time'
-    op_df = op_df.fillna(0)
+    out_df = pd.concat(dfs)
+    out_df.index.name = 'time'
+    out_df = out_df.fillna(0)
 
-    op_df['a_mag'] = [np.linalg.norm(x) for x in np.array(op_df[['ax', 'ay', 'az']])]
-    op_df['counts'] = [np.round(x) for x in op_df['a_mag'].rolling(5).mean()]
-    return op_df[['counts']]
+    out_df['a_mag'] = [np.linalg.norm(x) for x in np.array(out_df[['ax', 'ay', 'az']])]
+    out_df['counts'] = [np.round(x) for x in out_df['a_mag'].rolling(5).mean()]
+    return out_df[['counts']]
 
 
-def resample(df, new_fs):
+def resample(df: pd.DataFrame, new_fs: float) -> pd.DataFrame:
+    """
+    Resample different continuous segments of the given dataframe at the new 
+    sampling frequency.
+    """
     dfs = get_continuous_segments(df)
     dfs = [df.resample(f'{str(round(1 / new_fs, 2))}S', label='right', closed='right').mean()
            for df in dfs]
@@ -175,7 +157,40 @@ def resample(df, new_fs):
     return df
 
 
-def bandpass(x, fs=50, order=4):
+def bandpass(x: np.array, fs: float=50, order: int=4) ->  np.array:
+    """
+    Bandpass filter the signal between 0.25 and 2.5 Hz for computing the vector 
+    magnitude using the IMU data.
+    """
     sos = signal.butter(order, [0.25, 2.5], 'bandpass', fs=fs, output='sos', analog=False)
     filtered = signal.sosfilt(sos, x)
     return filtered
+
+
+
+
+# def get_largest_continuous_segment_indices(data: pd.DataFrame, subject: int,
+#                                            deltaT: np.timedelta64) -> tuple[int, int]:
+#     """
+#     Returns the indices of the longest continuous segment of data for the
+#     given subject.
+#     """
+#     _dtimes = np.diff(data[data.subject == subject].index)
+#     _jumpinx = np.hstack(([0], np.where(_dtimes > deltaT)[0]))
+#     _inx1 = np.argmax(np.diff(_jumpinx))
+#     return _jumpinx[_inx1], _jumpinx[_inx1 + 1] + 1
+
+
+# def get_segmented_data(data: pd.DataFrame, deltaT: np.timedelta64) -> dict[int, pd.DataFrame]:
+#     """
+#     Returns the segmented data for each subject in the given data.
+#     """
+#     # Go through each subject and get the longest continuous segments of data
+#     subjs = np.unique(data.subject)
+#     # Segment left data
+#     _subjdata = {}
+#     for _subj in subjs:
+#         # Get indices of the longest continuous segment of data
+#         inx = get_largest_continuous_segment_indices(data, _subj, deltaT)
+#         _subjdata[_subj] = data[data.subject == _subj].iloc[inx[0]:inx[1]]
+#     return _subjdata
